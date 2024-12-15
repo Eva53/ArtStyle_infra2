92b6877cbf441f6fa83167defcb97cb62becee87
Eva53 <efremenko2.va@edu.spbstu.ru>
2024-12-15 22:38:35 +0300

    changed

--- main.tf
+++ main.tf
@@ -1,58 +1,12 @@
-provider "openstack" {
-  auth_url    = var.auth_url
-  tenant_id   = var.project_id
-  username    = var.username
-  password    = var.password
-  region      = var.region_name
-  domain_name = var.user_domain_name
-}
-
-resource "openstack_compute_instance_v2" "efremenko_infra_tf" {
-  name            = var.instance_name
-  flavor_name     = var.flavor_name
-  image_name      = var.image_name
-  key_pair        = var.key_pair_name
-  security_groups = var.security_groups
+resource "openstack_compute_instance_v2" "my_instance" {
+  name        = var.instance_name
+  image_name  = var.image
+  flavor_name = var.flavor
+  key_pair    = var.key_name
 
   network {
-    port = openstack_networking_port_v2.port.id
+    name = var.network_name
   }
-}
 
-resource "openstack_networking_port_v2" "port" {
-  name           = "${var.instance_name}-port"
-  network_id     = var.network_id
-  admin_state_up = true
-}
-
-# Дождаться доступности сервера по SSH
-resource "null_resource" "wait_for_ssh" {
-  depends_on = [openstack_compute_instance_v2.efremenko_infra_tf]
-
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = "/home/user/.ssh/id_rsa"
-      host        = openstack_compute_instance_v2.efremenko_infra_tf.access_ip_v4
-    }
-
-    inline = [
-      "echo 'Server is accessible via SSH'",
-    ]
-  }
-}
-
-# Передать управление Ansible
-resource "null_resource" "ansible_provision" {
-  depends_on = [null_resource.wait_for_ssh]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      ansible-playbook -i ${openstack_compute_instance_v2.efremenko_infra_tf.access_ip_v4}, \
-        -u ubuntu \
-        --private-key=${"/home/user/.ssh/id_rsa"} \
-        playbook.yml
-    EOT
-  }
+  security_groups = [var.security_group]
 }
---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+
+  openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.32"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "openstack" {
+  auth_url    = "https://cloud.crplab.ru:5000"
+  tenant_name = "students"
+  user_domain_name = "Default"
+  user_name    = "master2022"
+  password    = "J8F3LGa*7KU7ye"
+  region      = "RegionOne"
+}
