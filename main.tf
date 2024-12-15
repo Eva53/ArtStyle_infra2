016d4d9dec1b67cac47026849ce1ee9c6a09bac5
Eva53 <efremenko2.va@edu.spbstu.ru>
2024-12-15 18:43:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "hashicorp/openstack"
+      version = "~> 1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "openstack" {
   auth_url    = var.auth_url
   tenant_id   = var.project_id
