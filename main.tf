c41cc24d8a60f0c68f3567ff908c85873fe7617b
Eva53 <efremenko2.va@edu.spbstu.ru>
2024-12-17 12:04:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,14 @@ resource "openstack_compute_instance_v2" "efremenko_infra_tf" {
     name = var.network_name
   }
 
+  block_device {
+    source_type           = "image"                        # Источник - образ
+    destination_type      = "volume"                       # Назначение - диск (volume)
+    uuid                  = "253a6ce9-fd9d-4a14-bbac-097a6eb8fb10"  # UUID образа
+    volume_size           = 20                             # Размер диска в ГБ
+    boot_index            = 0                              # Загрузочный диск
+    delete_on_termination = true
+  }
+
   security_groups = [var.security_group]
 }
-
-output "instance_ip" { value = openstack_compute_instance_v2.efremenko_infra_tf.access_ip_v4 }
